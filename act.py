338bfd82763b9713a4b8bb27aae3d7b3966d7f2c
SergioAle210 <ore221122@uvg.edu.gt>
2025-01-28 23:23:53 -0600

    FEAT: Se implementó el centinela y bufer para procesar el contenido

--- act.py
+++ act.py
@@ -7,28 +7,41 @@
 import re
 import csv
 
+
 # Función para leer el contenido de una página web desde un archivo local
-def leerPaginaWeb(archivoPagina):
-    with open(archivoPagina, 'r', encoding='utf-8') as archivo:
-        return archivo.read()
+def leerPaginaWebConBuffer(archivoPagina, tamanoBuffer=1024):
+    contenido = ""
+    with open(archivoPagina, "r", encoding="utf-8") as archivo:
+        while True:
+            bloque = archivo.read(tamanoBuffer)
+            if not bloque:  # Centinela: cuando no haya más datos
+                break
+            contenido += bloque
+    return contenido
+
 
 # Análisis del contenido HTML para obtener productos y sus imágenes
 def analizarHtmlYExtraerDatos(contenidoHtml):
-    expresionRegular = re.compile(r'<div class="caption col-12">.*?<p class="pr-4">(.*?)</p>.*?<div class="overflow-hidden".*?<img.*?src="(.*?)"', re.DOTALL)
+    expresionRegular = re.compile(
+        r'<div class="caption col-12">.*?<p class="pr-4">(.*?)</p>.*?<div class="overflow-hidden".*?<img.*?src="(.*?)"',
+        re.DOTALL,
+    )
     listaProductos = expresionRegular.findall(contenidoHtml)
     return listaProductos
 
+
 # Guarda los datos extraídos en un archivo CSV
 def guardarEnCsv(datosProductos, nombreArchivo):
-    with open(nombreArchivo, 'w', newline='', encoding='utf-8') as archivoCsv:
+    with open(nombreArchivo, "w", newline="", encoding="utf-8") as archivoCsv:
         manejadorCsv = csv.writer(archivoCsv)
         # Encabezados del archivo CSV
-        manejadorCsv.writerow(['Nombre del Producto', 'URL de la Imagen'])  
+        manejadorCsv.writerow(["Nombre del Producto", "URL de la Imagen"])
         manejadorCsv.writerows(datosProductos)  # Datos de productos
 
+
 # Inicia el proceso de carga, análisis y guardado
-contenidoPagina = leerPaginaWeb('pagina.html')
+contenidoPagina = leerPaginaWebConBuffer("pagina.html")
 productosEncontrados = analizarHtmlYExtraerDatos(contenidoPagina)
-guardarEnCsv(productosEncontrados, 'listadoProductos.csv')
+guardarEnCsv(productosEncontrados, "listadoProductos.csv")
 
 print("CSV creado")
